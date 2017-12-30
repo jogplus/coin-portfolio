a0e71048c2b6ca8413d3e20d80befed4d9fd6ef9
Tristan Jogminas <tristan.jogminas@gmail.com>
2017-12-29 16:28:43 -0800

    ordered coins

--- coin_script.js
+++ coin_script.js
@@ -1,10 +1,20 @@
 $(function(){
     $("button").click(function(){
         $.get("https://api.coinmarketcap.com/v1/ticker/", function(data){
-            var coinInfo = data[0]
-            var bitcoin = JSON.stringify(coinInfo)
-            var bitcoin = JSON.parse(bitcoin)
-            document.getElementById("prices").innerHTML = bitcoin.name
+            var table = document.getElementById("myTable");
+            for (coinIndex = 0; coinIndex < data.length; coinIndex++){
+            var coinJSON = JSON.stringify(data[coinIndex])
+            var coinInfo = JSON.parse(coinJSON)
+            var row = table.insertRow(table.length);
+            var coinRank = row.insertCell(0);
+            var coinName = row.insertCell(1);
+            var coinPrice = row.insertCell(2);
+            var coinChange = row.insertCell(3);
+            coinRank.innerHTML = coinInfo.rank;
+            coinName.innerHTML = coinInfo.name;
+            coinPrice.innerHTML = coinInfo.price_usd;
+            coinChange.innerHTML = coinInfo.percent_change_24h;
+            }
         })
     })
  });
