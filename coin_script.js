b96add24120e0682b480e8b53f9c1cd86e62cadf
Tristan Jogminas <tristan.jogminas@gmail.com>
2017-12-30 22:48:43 -0800

    centered titles

--- coin_script.js
+++ coin_script.js
@@ -11,7 +11,7 @@ $(function(){
                 var coinChange = row.insertCell(3);
                 coinRank.innerHTML = coinInfo.rank;
                 coinName.innerHTML = coinInfo.name;
-                coinPrice.innerHTML = coinInfo.price_usd;
+                coinPrice.innerHTML = '$' + coinInfo.price_usd;
                 if (coinInfo.percent_change_24h.substring(0,1) == '-'){
                     coinChange.innerHTML = '-' + coinInfo.percent_change_24h.substring(1, coinInfo.percent_change_24h.length) + '%';
                     coinChange.style.color = '#FF6B6B';
@@ -22,15 +22,5 @@ $(function(){
                 }
                 coinChange.style.fontWeight = 'bolder';
             }
-            var titleRow = table.insertRow(0);
-            var tableTitle = titleRow.insertCell(0);
-            tableTitle.colSpan = 4;
-            tableTitle.style.textAlign = "center";
-            tableTitle.style.color = '#FF6B6B';
-            tableTitle.style.backgroundColor = '#8EDFD9'
-            tableTitle.style.fontFamily = 'Pacifico';
-            tableTitle.style.fontSize = '40px';
-            tableTitle.style.textShadow = "-1px -1px 0 #F7FFF7,1px -1px 0 #F7FFF7,-1px 1px 0 #F7FFF7,1px 1px 0 #F7FFF7,-2px 0 0 #F7FFF7,2px 0 0 #F7FFF7,0 2px 0 #F7FFF7,0 -2px 0 #F7FFF7";
-            tableTitle.innerHTML ='Current Coin Rankings';
     })
  });
