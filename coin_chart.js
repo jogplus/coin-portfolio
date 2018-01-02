67a8bd5b3ee79aecd8cc65d0ad50e61b2aac112f
Tristan Jogminas <tristan.jogminas@gmail.com>
2018-01-01 23:09:33 -0800

    remove coin button

--- coin_chart.js
+++ coin_chart.js
@@ -5,11 +5,9 @@ var tableData = [['Coin', 'Money Invested'],['Bitcoin',11],['Litecoin',2],['Ripp
 var example = true;
 
 function drawChart() {
-
     var data = google.visualization.arrayToDataTable(
         tableData
     );
-
     var options = {
     backgroundColor: '#F7FFF7',
     fontName: 'Open Sans',
@@ -17,11 +15,7 @@ function drawChart() {
     chartArea: {'width': '100%', 'height': '90%'},
     slices: [{color:'#4ECDC4'},{color:'#1A535C'},{color:'#FF6B6B'},{color:'#FFBA49'}]
     };
-
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
     chart.draw(data, options);
-
     document.getElementById('totalMoney').innerHTML= "Total: $100"
-
 }
--- coin_script.js
+++ coin_script.js
@@ -1,26 +1,27 @@
-$(function getTable(){
-        $.get("https://api.coinmarketcap.com/v1/ticker/", function(data){
-            var table = document.getElementById("myTable");
-            for (coinIndex = 0; coinIndex < 20; coinIndex++){
-                var coinJSON = JSON.stringify(data[coinIndex])
-                var coinInfo = JSON.parse(coinJSON);
-                var row = table.insertRow(table.length);
-                var coinRank = row.insertCell(0);
-                var coinName = row.insertCell(1);
-                var coinPrice = row.insertCell(2);
-                var coinChange = row.insertCell(3);
-                coinRank.innerHTML = coinInfo.rank;
-                coinName.innerHTML = coinInfo.name + ' (' + coinInfo.symbol + ')';
-                coinPrice.innerHTML = '$' + coinInfo.price_usd;
-                if (coinInfo.percent_change_24h.substring(0,1) == '-'){
-                    coinChange.innerHTML = '-' + coinInfo.percent_change_24h.substring(1, coinInfo.percent_change_24h.length) + '%';
-                    coinChange.style.color = '#FF6B6B';
-                }
-                else{
-                    coinChange.innerHTML = '+' + coinInfo.percent_change_24h + '%';
-                    coinChange.style.color = '#61CA00';
-                }
-                coinChange.style.fontWeight = 'bolder';
-            }
-    })
- });
+var apiData;
+
+$.get("https://api.coinmarketcap.com/v1/ticker/", function(data){
+    apiData = data
+    var table = document.getElementById("myTable");
+    for (coinIndex = 0; coinIndex < 20; coinIndex++){
+        var coinJSON = JSON.stringify(apiData[coinIndex])
+        var coinInfo = JSON.parse(coinJSON);
+        var row = table.insertRow(table.length);
+        var coinRank = row.insertCell(0);
+        var coinName = row.insertCell(1);
+        var coinPrice = row.insertCell(2);
+        var coinChange = row.insertCell(3);
+        coinRank.innerHTML = coinInfo.rank;
+        coinName.innerHTML = coinInfo.name + ' (' + coinInfo.symbol + ')';
+        coinPrice.innerHTML = '$' + coinInfo.price_usd;
+        if (coinInfo.percent_change_24h.substring(0,1) == '-'){
+            coinChange.innerHTML = '-' + coinInfo.percent_change_24h.substring(1, coinInfo.percent_change_24h.length) + '%';
+            coinChange.style.color = '#FF6B6B';
+        }
+        else{
+            coinChange.innerHTML = '+' + coinInfo.percent_change_24h + '%';
+            coinChange.style.color = '#61CA00';
+        }
+        coinChange.style.fontWeight = 'bolder';
+    }
+});
